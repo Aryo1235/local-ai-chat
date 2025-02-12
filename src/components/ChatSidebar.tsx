6fe3de13e8fd4ab766b993ea0154f29d10e49f8f
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2025-02-12 15:39:07 +0700

    update side bar

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -14,17 +14,30 @@ import {
   Sidebar as SidebarPrimitive,
 } from "~/components/ui/sidebar";
 import { useTheme } from "./ThemeProvider";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { db } from "~/lib/dexie";
+import { useLiveQuery } from "dexie-react-hooks";
 
-const chatGroups = [
-  { id: "1", name: "React Basics" },
-  { id: "2", name: "AI Ethics" },
-  { id: "3", name: "Climate Change" },
-  { id: "4", name: "JavaScript Tips" },
-  { id: "5", name: "Machine Learning Intro" },
-];
+// const chatGroups = [
+//   { id: "1", name: "React Basics" },
+//   { id: "2", name: "AI Ethics" },
+//   { id: "3", name: "Climate Change" },
+//   { id: "4", name: "JavaScript Tips" },
+//   { id: "5", name: "Machine Learning Intro" },
+// ];
 
 export const ChatSidebar = () => {
   const [activeChat, setActiveChat] = useState<string | null>(null);
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const [textInput, setTextInput] = useState("");
   const { setTheme, theme } = useTheme();
 
   const handleToggleTheme = () => {
@@ -34,45 +47,84 @@ export const ChatSidebar = () => {
       setTheme("dark");
     }
   };
+  const threads = useLiveQuery(() => db.getAllThreads(), []);
+
+  const handleCreateThread = async () => {
+    const threadId = await db.createThread(textInput);
+
+    setDialogIsOpen(false);
+    setTextInput("  ");
+  };
 
   return (
-    <SidebarPrimitive>
-      <SidebarHeader>
-        <Button className="w-full justify-start" variant="ghost">
-          <Plus className="mr-2 h-4 w-4" />
-          New Chat
-        </Button>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarGroupLabel>Recent Chats</SidebarGroupLabel>
-            <SidebarMenu>
-              {chatGroups.map((chat) => (
-                <SidebarMenuItem key={chat.id}>
-                  <SidebarMenuButton
-                    onClick={() => setActiveChat(chat.id)}
-                    isActive={activeChat === chat.id}
-                  >
-                    {chat.name}
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <Button
-          onClick={handleToggleTheme}
-          variant="ghost"
-          className="w-full justify-start"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />{" "}
-          Toggle Theme
-        </Button>
-      </SidebarFooter>
-    </SidebarPrimitive>
+    <>
+      <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create a new chat</DialogTitle>
+          </DialogHeader>
+
+          <div className="space-y-1">
+            <Label htmlFor="thread-title">Thread tittle </Label>
+            <Input
+              id="thread-title"
+              value={textInput}
+              onChange={(e) => {
+                setTextInput(e.target.value);
+              }}
+              placeholder="Enter your Threads.."
+            />
+          </div>
+          <DialogFooter>
+            <Button variant="secondary" onClick={() => setDialogIsOpen(false)}>
+              Cancel
+            </Button>
+            <Button onClick={handleCreateThread}> Save </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <SidebarPrimitive>
+        <SidebarHeader>
+          <Button
+            onClick={() => setDialogIsOpen(true)}
+            className="w-full justify-start"
+            variant="ghost"
+          >
+            <Plus className="mr-2 h-4 w-4" />
+            New Chat
+          </Button>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarGroupLabel>Recent Chats</SidebarGroupLabel>
+              <SidebarMenu>
+                {threads?.map((thread) => (
+                  <SidebarMenuItem key={thread.id}>
+                    <SidebarMenuButton
+                      onClick={() => setActiveChat(thread.id)}
+                      isActive={activeChat === thread.id}
+                    >
+                      {thread.title}
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <Button
+            onClick={handleToggleTheme}
+            variant="ghost"
+            className="w-full justify-start"
+          >
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />{" "}
+            Toggle Theme
+          </Button>
+        </SidebarFooter>
+      </SidebarPrimitive>
+    </>
   );
 };
