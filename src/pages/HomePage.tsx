27aa956d0f0c13dad23b808e4522b80cfc379102
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2025-03-12 15:48:30 +0700

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center ">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Chat App</h1>
+      <h1 className="text-3xl font-bold mb-4">Welcome Back ARYO</h1>
     </div>
   );
 }
