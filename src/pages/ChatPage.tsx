2d01515d9a39290b9cf57350256a66864ee39658
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2025-03-12 17:05:54 +0700

    Update ChatPage.tsx

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -40,7 +40,7 @@ export default function ChatPage() {
     });
 
     const stream = await ollama.chat({
-      model: "deepseek-r1:1.5b",
+      model: "deepseek-r1:8b",
       messages: [
         {
           role: "user",
